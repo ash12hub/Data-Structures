dbeb31b29a700afa4da584c807bd7fa92b02f0e1
ash12hub <ash12hub@users.noreply.github.com>
2020-05-21 09:05:06 +0530

    Added Stack

--- stack/stack.py
+++ stack/stack.py
@@ -76,3 +76,16 @@ class List_Node:
         self.value = value
         self.next_val = next_val
         self.prev_val = prev_val
+
+"""
+When using a Linked List the next and previous values need to be changed when
+adding a new element, whereas, when using an array, the is index automatically
+updated whenever a new value is added or removed from a list. When adding to an
+array, the append() method can be used to add a value to the end. And the
+remove() method can automatically remove a specified value. The [-1] index can
+be used to automatically ge the value at the end, making it easy to remove it.
+In a Linked List, a value is assigned to a tail and when that is removed, the
+previous value will have no next value and that will be the new tail. When
+adding a new tail, the new node has to be added to the next value of the
+current tail, in order to add it to the end.
+"""
